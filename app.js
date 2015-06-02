e801fc4c7c2f11a4cf8213f3a8e6ada8492fb611
unknown <aaravind@LC3000043270.INTRANET.SAKSROOT.SAKSINC.com>
2015-06-02 14:57:25 -0400

    tt

--- app.js
+++ app.js
@@ -22,15 +22,46 @@ var io = require('socket.io').listen(app.listen(port));
 require('./config')(app, io);
 require('./routes')(app, io);
 var pg = require('pg');
-var conString = "postgres://otadthilhxfiqv:sVgqucfFBBijtv9fFKaEAGnAIa@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d59eb19fu1hs81";
+/*var conString = "postgres://otadthilhxfiqv:sVgqucfFBBijtv9fFKaEAGnAIa@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d59eb19fu1hs81";
 var dataclinet = new pg.Client(conString);
 dataclinet.connect();
-dataclinet.query('SELECT * FROM test', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { console.log(result.rows); }
+var query = dataclinet.query("select * from employee");
+       // query.on("row", function (row, result) { 
+         //   result.addRow(row); 
+        //});
+	*/	
+		
+var dataclinet = new pg.Client({
+    user: "otadthilhxfiqv",
+    password: "sVgqucfFBBijtv9fFKaEAGnAIa",
+    database: "d59eb19fu1hs81",
+    port: 5432,
+    host: "ec2-54-163-238-96.compute-1.amazonaws.com",
+    ssl: true
 });
+/*
+var dataclinet = new pg.Client({
+    user: "post",
+    password: "post",
+    database: "postgres",
+    port: 5432,
+    host: "localhost",
+    ssl: false
+});
+*/
+dataclinet.connect();
+
+var query = dataclinet.query("select * from test;");
+        query.on("row", function (row, result) { 
+            result.addRow(row); 
+        });
+        query.on("end", function (result) {          
+            dataclinet.end();
+            //res.writeHead(200, {'Content-Type': 'text/plain'});
+            //res.write(JSON.stringify(result.rows, null, "    ") + "\n");
+            //res.end();  
+			console.log(result.rows);
+	
+        });	
 console.log('Your application is running on http://localhost:' + port);
 console.log('Your application is running on http://localhost:' + port);
